04b0aceee8941523d28780c917b2b38fbd1856c6
Diepreye D. Charles-Daniel <davediepreye05@gmail.com>
2024-07-09 12:17:11 -0600

    Update

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     hamburgerMenu.classList.toggle('open');
     hamburgerIcon.classList.toggle('open');
 }
+
+function toggleYear(index) {
+
+    // Remove selected-year class from all years
+    // Each year has class 'year-0', 'year-1', 'year-2', 'year-3', 'year-4', 'year-5'
+    const years = document.querySelectorAll('li[class^="year-"]');
+    years.forEach((year) => {
+        if (!year.classList.contains(`year-${index}`)) {
+            year.classList.remove('selected-year');
+        }
+    });
+
+    const year = document.querySelector(`.year-${index}`);
+    year.classList.toggle('selected-year');
+}
